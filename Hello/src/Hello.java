9ecb4ecd42610ddbf260de702233a0f7ccac49ed
MinhQuan1808 <145845509+MinhQuan1808@users.noreply.github.com>
2025-02-12 19:09:24 +0700

    test push tu IDE len Server

---
+++ Hello/src/Hello.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// test push tu IDE len Server
+	}
+
+}
