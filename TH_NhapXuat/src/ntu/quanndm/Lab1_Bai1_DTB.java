d33bfafe5cfbfc1965efda3bdcbe16ab25b1fb19
MinhQuan1808 <145845509+MinhQuan1808@users.noreply.github.com>
2025-02-16 18:37:07 +0700

    Chuan bi lam lab 1, bai 1

---
+++ TH_NhapXuat/src/ntu/quanndm/Lab1_Bai1_DTB.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package ntu.quanndm;
+
+/**
+ * 
+ */
+public class Lab1_Bai1_DTB {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
