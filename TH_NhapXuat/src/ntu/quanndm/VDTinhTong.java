942f76ddfe2bdc795cb096ad151fe78fde2b11ca
MinhQuan1808 <145845509+MinhQuan1808@users.noreply.github.com>
2025-02-16 18:31:21 +0700

    Full code vi du tinh tong, test OK

---
+++ TH_NhapXuat/src/ntu/quanndm/VDTinhTong.java
@@ -0,0 +1,21 @@
+package ntu.quanndm;
+
+import java.util.Scanner;
+
+public class VDTinhTong {
+	public static void main(String[] args) {
+		//In dong tieu de
+		System.out.println("VI DU TINH TONG");
+		//Chuan bi cho viec nhap
+		Scanner banPhim = new Scanner(System.in);
+		//Huong dan nhap
+		System.out.println("Nhap A: ");
+		double a = banPhim.nextDouble();
+		System.out.println("Nhap B: ");
+		double b = banPhim.nextDouble();
+		//Xuat tong
+		double tong = a + b;
+		System.out.print("Tong la: ");
+		System.out.print(tong);
+	}
+}
