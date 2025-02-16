4fc0e4c3d13b17a37022b2fe0e8cd9fa0648c1c3
MinhQuan1808 <145845509+MinhQuan1808@users.noreply.github.com>
2025-02-16 18:03:36 +0700

    Update Hello.java

--- Hello/src/Hello.java
+++ Hello/src/Hello.java
@@ -12,7 +12,6 @@ public class Hello {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		// test push tu IDE len Server
+		System.out.println("Hello world");
 	}
-
 }
