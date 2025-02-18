cd1421245c670265bf486c5c2287b4293b759abc
MinhQuan1808 <145845509+MinhQuan1808@users.noreply.github.com>
2025-02-18 10:11:36 +0700

    Cap nhat vi du huong doi tuong lan 2

--- ViDuHuongDoiTuong/src/ntu/quanndm/SanPham.java
+++ ViDuHuongDoiTuong/src/ntu/quanndm/SanPham.java
@@ -10,12 +10,25 @@ public class SanPham {
 	public SanPham() {
 		// Khoi tao gia tri ban dau cho cac thuoc tinh neu co o day
 	}
-	
-	public SanPham(int _maSP, String _tenSP, String _loaiSP, String _anhSP) {
+//	public SanPham(int _maSP, String _tenSP, String _loaiSP, String _anhSP) {
 		// Khoi tao gia tri ban dau cho cac thuoc tinh neu co o day
-		maSP = _maSP;
-		tenSP = _tenSP;
-		loaiSP = _loaiSP;
-		anhSP = _anhSP;
+//		maSP = _maSP;
+//		tenSP = _tenSP;
+//		loaiSP = _loaiSP;
+//		anhSP = _anhSP;
+//	}
+	public SanPham(int maSP, String tenSP, String loaiSP, String anhSP) {
+		// Khoi tao gia tri ban dau cho cac thuoc tinh neu co o day
+		this.maSP = maSP;
+		this.tenSP = tenSP;
+		this.loaiSP = loaiSP;
+		this.anhSP = anhSP;
+	}
+	//Getter, Setter
+	public int getMaSP() {
+		return maSP;
+	}
+	public void setMaSP(int maSP) {
+		this.maSP = maSP;
 	}
 }
